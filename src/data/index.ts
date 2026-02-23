f8571cb4113852ef48b57b93e08ca6e9465f5de3
Abhijeet Kumar <abhijeetk829@gmail.com>
2026-02-24 02:30:45 +0530

    data changes

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,6 @@
 import data1 from "./data_1.json";
 import data2 from "./data_2.json";
-import data3 from "./data_3_cleaned.json";
+import data3 from "./data_3.json";
 
 const combinedData = [...data1, ...data2, ...data3];
 
